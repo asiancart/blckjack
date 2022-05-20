47eff878d6e9dd677f3c0e60ecfd1414fea33e11
asiancart <asiancart@hotmail.com>
2022-05-21 00:08:32 +0300

    birthday paradox

--- main.py
+++ main.py
@@ -1,71 +1,78 @@
-import random
+import datetime , random
 
-NUM_DIGITS = 3
-MAX_GUESSES = 10
+def getBirthdays(numbersOfBirthdays):
+    birthdays = []
 
+    for i in range(numbersOfBirthdays):
+        startOfYear = datetime.date(2001,1,1)
 
-def main():
-    print('''Bagels, a deductive logic game.''')
-
-    while True:
-        secretNum = getSecretNum()
-        print('I have thought up a number.it is 3 digit number(can start with 0).')
-        print('You have {} guesses to get it.'.format(MAX_GUESSES))
-
-        numGuesses = 1
-        while numGuesses <= MAX_GUESSES:
-            guess = ''
-            while len(guess) != NUM_DIGITS or not guess.isdecimal():
-                print('Guess #{}: '.format(numGuesses))
-                guess = input('> ')
-
-            clues = getClues(guess,secretNum)
-            print(clues)
-            numGuesses += 1
-
-            if guess == secretNum:
-                break
-            if numGuesses> MAX_GUESSES:
-                print("You ran out of guesses.")
-                print('The answer was {}'.format(secretNum))
+        randomNumberOfDays = datetime.timedelta(random.randint(0,364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
 
 
-        print('Do you want to play again ? (yes or no)')
-        if not input('> ').lower().startswith('y'):
-            break
+def getMatch(birthdays):
+    if len(birthdays) == len(set(birthdays)):
+        return None
 
-    print("thanks for playing.")
+    for a , birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[a+1:]):
+            if birthdayA == birthdayB:
+                return birthdayA
 
+print('The Birthday Paradox shows us that in a group of N people, the odds that two of them have matching birthdays is surprisingly large.This program does a Monte Carlo simulation (that is, repeated random simulations) to explore this concept.')
 
-def getSecretNum():
-    numbers = list('0123456789')
-    random.shuffle(numbers)
+MONTHS = ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
 
-    secretNum = ''
-    for i in range(NUM_DIGITS):
-        secretNum += str(numbers[i])
-    return secretNum
+while True:
+    print("How many birthdays shall I generate? (Max 100)")
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <=100):
+        numBDays = int(response)
+        break
+print()
 
+print("Here are", numBDays , "birthdays:")
+birthdays = getBirthdays(numBDays)
+for i , birthday in enumerate(birthdays):
+    if i != 0:
+        print(', ', end='')
 
-def getClues(guess,secretNum):
+    monthName= MONTHS[birthday.month-1]
+    dateText = ' {} {} '.format(monthName,birthday.day)
+    print(dateText,end='')
+print()
+print()
 
-    if guess == secretNum:
-        return 'You got it.'
+match=getMatch(birthdays)
 
-    clues = []
+print('In this simulation, ',end='')
+if match != None:
+    monthName = MONTHS[match.month - 1]
+    dateText = '{} {}'.format(monthName, match.day)
+    print('multiple people have a birthday on', dateText)
+else:
+    print('there are no matching birthdays')
+print()
 
-    for i in range(len(guess)):
-        if guess[i] == secretNum[i]:
-            clues.append('One digit is correct and in the right position.')
-        elif guess[i] in secretNum:
-            clues.append('One digit is correct and in the wrong position.')
+print('Generating', numBDays, 'random birthdays 100,000 times...')
+input('Press Enter to begin...')
 
+print('Let\'s run another 100,000 simulations.')
+simMatch = 0
+for i in range(100000):
+    if i % 10000 == 0 :
+        print(i, 'simulations run...')
+    birthdays= getBirthdays(numBDays)
+    if getMatch(birthdays) != None:
+        simMatch += 1
 
-    if len(clues) == 0:
-        return 'There are no digits correct at all.'
-    else:
-        clues.sort()
-        return ''.join(clues)
+print('100,000 simulations run.')
 
-if __name__ == '__main__':
-    main()
+probability = round(simMatch / 100000 * 100, 2)
+print('Out of 100,000 simulations of', numBDays, 'people, there was a')
+print('matching birthday in that group', simMatch, 'times. This means')
+print('that', numBDays, 'people have a', probability, '% chance of')
+print('having a matching birthday in their group.')
+print('That\'s probably more than you would think!')
